e9803212d2d8b1aaca1ee2aa0106539f5dfc8ac4
IshanMK <e18173@eng.pdn.ac.lk>
2024-06-28 16:34:09 +0530

    Fixed

--- spam_classifier_app.py
+++ spam_classifier_app.py
@@ -1,38 +1,55 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import re
-import string
 import nltk
-from text_processing import clean_text, count_punct
+from nltk.stem import PorterStemmer
 import os
 import sys
+from text_processing import clean_text, count_punct  # Adjust this import based on your file structure
+
+# Set the page configuration
+st.set_page_config(page_title="Spam Email Classifier")
 
 # Determine the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Add script directory to Python path
-sys.path.append(script_dir)
 
 # Set NLTK data path to include the current script directory
 nltk_data_dir = os.path.join(script_dir, "nltk_data")
 nltk.data.path.append(nltk_data_dir)
 
+# Path to the directory containing stopwords
+stopwords_dir = os.path.join(script_dir, "nltk_data", "corpora", "stopwords" , "stopwords")
+
+# Load NLTK stopwords and stemmer
+ps = PorterStemmer()
+
+# Check if NLTK stopwords are already available
+if not os.path.exists(stopwords_dir):
+    st.error("NLTK stopwords directory not found. Please ensure it exists and contains the necessary files.")
+    st.stop()
+
+# Set NLTK data path specifically for stopwords
+nltk.data.path.append(stopwords_dir)
+
+# Ensure NLTK resources are available
+try:
+    nltk.data.find('corpora/stopwords/stopwords')
+except LookupError:
+    st.warning("NLTK stopwords not found. Please download NLTK data or ensure it is correctly configured.")
+    st.stop()
+
+# Load NLTK stopwords from the specified directory
+with open(os.path.join(stopwords_dir, 'english'), 'r') as stopwords_file:
+    stopwords = stopwords_file.read().splitlines()
+
 # Load the pre-trained model and vectorizer
 model_path = os.path.join(script_dir, 'spam_classifier_model.pkl')
 vectorizer_path = os.path.join(script_dir, 'tfidf_vectorizer.pkl')
 
-# nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('english')
-ps = nltk.PorterStemmer()
-
-# Set the page configuration
-st.set_page_config(page_title="Spam Email Classifier")
-
-# Load the pre-trained model and vectorizer
-with open('spam_classifier_model.pkl', 'rb') as model_file:
+with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
-with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+
+with open(vectorizer_path, 'rb') as vectorizer_file:
     tfidf_vect = pickle.load(vectorizer_file)
 
 # Streamlit app
